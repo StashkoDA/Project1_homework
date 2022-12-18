058782b0c7b591a5999cf59e23db77f2e78ff094
Dmitrii_Stashko <1028047@mail.ru>
2022-12-18 20:25:34 +1000

    Добавил решение задачи

---
+++ project1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+void PrintArray(string[] arrString)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrString.Length - 1; i++)
+    {
+        Console.Write(arrString[i] + ", ");
+    }
+    Console.Write(arrString[arrString.Length - 1] + "] -> ");
+}
+
+string[] GetMassive(string[] arrString)
+{
+    int j = 0;
+    for (int i = 0; i < arrString.Length; i++)
+    {
+        string message = arrString[i];
+        if (message.Length < 4) j++;
+    }
+    
+    string[] resArr = new string[j];
+    int q = 0;
+    for (int k = 0; k < arrString.Length; k++)
+    {
+        string message = arrString[k];
+        if (message.Length < 4)
+        {
+            resArr[q] = arrString[k];
+            q++;
+
+        }
+    }
+    return resArr;
+}
+
+void PrintResult(string[] arrString)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrString.Length - 1; i++)
+    {
+        if (arrString[i] != String.Empty)
+        {
+            Console.Write(arrString[i] + ",");
+        }
+    }
+    Console.Write(arrString[arrString.Length - 1] + "]");
+    Console.WriteLine();
+}
+
+string[] arrayString = { "qwer", "a6s", "(*)", "1@", "$", "next" };
+PrintArray(arrayString);
+string[] resultArr = GetMassive(arrayString);
+PrintResult(resultArr);
